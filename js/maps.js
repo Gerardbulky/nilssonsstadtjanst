85afc97cb0c77f2e7413c9d8116352ad62b3f50a
gerardambe <gerardambe@yahoo.com>
2020-06-14 08:19:19 +0000

    added contact informations

---
+++ js/maps.js
@@ -0,0 +1,8 @@
+function initMap() {
+
+        var options = {
+          zoom: 3,
+          center: {lat: 59.1327, lng: 12.9301}
+        }
+        var map = new google.maps.Map(document.getElementById('map'), options);
+    }
