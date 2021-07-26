54a1b51394ee6f6e25e7458067753e738734b70f
gerardambe <gerardambe@yahoo.com>
2021-07-26 21:31:21 +0000

    added cleaning category menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
     $('#myCollapsible').collapse('show')
     $('#element').toast('show')
+    
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
 })
